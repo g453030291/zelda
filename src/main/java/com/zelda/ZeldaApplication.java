b3bab83cb85c622a0ddbda8b91594c0910960aa1
g453030291 <453030291@qq.com>
2020-03-25 08:40:51 +0800

    update resource

--- src/main/java/com/zelda/ZeldaApplication.java
+++ src/main/java/com/zelda/ZeldaApplication.java
@@ -2,12 +2,18 @@ package com.zelda;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ZeldaApplication {
+public class ZeldaApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ZeldaApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ZeldaApplication.class);
+	}
 }
